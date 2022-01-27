534f44a85843d396541da40c9e9ea0aa02e701fa
Aleksey Bragin <ab@alexbragin.com>
2022-01-27 14:42:51 +0300

    Implement nsu_getmonotonic_ms() for Win32 platform

--- src/time.c
+++ src/time.c
@@ -46,6 +46,8 @@ nsuerror nsu_getmonotonic_ms(uint64_t *current_out)
 
     clock_gettime(CLOCK_MONOTONIC, &tp);
     current = (tp.tv_sec * 1000) + (tp.tv_nsec / 1000000);
+#elif defined(WIN32)
+    current = GetTickCount64();
 #elif defined(__riscos)
     os_t time;
 
